5a5a50ac15914a22783e1e77be59ae42fe6b51ab
shivangijoshi23022 <shivangi.joshi@trijotech.com>
2024-12-05 23:04:05 +0530

    Update Home.js

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -10,7 +10,7 @@ function Home() {
 
     useEffect(() => {
         if (!user) {
-            axios.get('http://46.202.162.187:3001/user', { withCredentials: true })
+            axios.get('http://localhost:3001/user', { withCredentials: true })
                 .then(response => {
                     if (response.data.user) {
                         setUser(response.data.user);
