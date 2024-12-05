8b0477cb57cef7345efefb96b481b23fcc4975f7
shivangijoshi23022 <shivangi.joshi@trijotech.com>
2024-12-05 22:24:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend's URL
+    origin: 'http://46.202.162.187:3000', // Replace with your frontend's URL
     credentials: true
 }));
 
